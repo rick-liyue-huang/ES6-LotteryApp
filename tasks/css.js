8499405106bc26b802f09787bc0054b5e76cd6f6
rickliyuehuang <rick.liyue.huang@gmail.com>
2017-06-27 13:53:33 +1000

    test

--- tasks/css.js
+++ tasks/css.js
@@ -7,9 +7,9 @@ import args from './util/args';
 
 gulp.task('css', () => {
 
-	return gulp.src('app/**/*.css')
+	return gulp.src('app/css/*.css')
 
-		.pipe(gulp.dest('server/public/css/'))
+		.pipe(gulp.dest('server/public/css'))
 		.pipe(gulpif(args.watch, livereload()));
 
 });
